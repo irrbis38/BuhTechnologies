f78d9865fefce6c799793f93e9531c06d9e6bf9f
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2021-07-20 17:12:18 +0300

    feat: creat reviews block

--- js/main.js
+++ js/main.js
@@ -20,3 +20,53 @@ const activateUnabilityButtons = () => {
 
 abilityBtn.addEventListener('click', activateAbilityButtons);
 unabilityBtn.addEventListener('click', activateUnabilityButtons);
+
+
+// аккордеон в reviews
+
+let reviewArrows = document.querySelectorAll(".review__arrow");
+
+for (let i = 0; i < reviewArrows.length; i++) {
+  reviewArrows[i].addEventListener("click", function() {
+    let panel = this.previousElementSibling;
+    if (panel.classList.contains('active')) {
+      panel.style.maxHeight = "134px";
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";      
+    }
+    this.classList.toggle("active");
+    panel.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("active");
+  });
+}
+
+// благодарственное письмо на весь экран
+
+let reviewHovers = document.querySelectorAll(".reviews__hover");
+
+let reviewsBlackBlackgrounds = document.querySelectorAll(".reviews__black-bg");
+
+let reviewsFullSizeImages = document.querySelectorAll(".review__img-fullscreen");
+
+let reviewCloseAll = document.querySelectorAll(".review__close");
+
+for (let i = 0; i < reviewHovers.length; i++) {
+  reviewHovers[i].addEventListener("click", function() {
+    reviewsBlackBlackgrounds[i].classList.add("active");
+    reviewsFullSizeImages[i].classList.add("active");
+  });
+}
+
+for (let i = 0; i < reviewsBlackBlackgrounds.length; i++) {
+  reviewsBlackBlackgrounds[i].addEventListener("click", function() {
+    reviewsBlackBlackgrounds[i].classList.remove("active");
+    reviewsFullSizeImages[i].classList.remove("active");
+  });
+}
+
+for (let i = 0; i < reviewCloseAll.length; i++) {
+  reviewCloseAll[i].addEventListener("click", function() {
+    reviewsBlackBlackgrounds[i].classList.remove("active");
+    reviewsFullSizeImages[i].classList.remove("active");
+  });
+}
